dc9ec26e1d3fe36259cbb9a5bb9e2df41ffa5da5
tanulo <tanulo@barossgabor.local>
2022-01-04 13:32:11 +0100

    01-04

--- Tricepsz.js
+++ Tricepsz.js
@@ -1,24 +1,77 @@
-import React, { Component } from 'react';
-import { Text, TextInput, View, Image } from 'react-native';
+import React from 'react';
 
-export default class Tricepsz extends Component {
-  constructor(props) {
+import {StyleSheet, FlatList, ActivityIndicator, Text, View, Image , TouchableOpacity } from 'react-native';
+
+export default class Tricepsz extends React.Component {
+
+  constructor(props){
     super(props);
-    this.state = {text: ''};
+    this.state ={
+       isLoading: true,
+       dataSource:[]
+    }
   }
 
-  render() {
-    
-    return (
-      <View style={{padding: 10}}>
-       
-        <Text style={{padding: 10, fontSize: 42}}>
-         Tricepsz
-        </Text>
-       
+  componentDidMount(){
+    return fetch('http://192.168.1.106:3000/gyakorlatok_tricepsz')
+      .then((response) => response.json())
+      .then((responseJson) => {
+
+        this.setState({
+          isLoading: false,
+          dataSource: responseJson,
+        }, function(){
+
+        });
+
+      })
+      .catch((error) =>{
+        console.error(error);
+      });
+  }
+
+
+
+
+  
+
+
+  render(){
+
+  
+
+    return(
+      <View style={{ backgroundColor:"#DCF9F4"}}>
+        <FlatList
+          data={this.state.dataSource}
+          renderItem={({item}) => 
+
+          /*<View style={{ border: "solid blue",width:600, marginLeft:"auto",marginRight:"auto",padding:20,marginBottom:10,borderRadius:20,}}>*/
+          <View style={{marginLeft:"auto",marginRight:"auto",padding:40,marginBottom:10,  border: "solid blue", borderRadius:60,backgroundColor: "#B9F3EA"}}>
+          <Text style={{color:"brown",fontSize:40,textAlign:"center",marginTop:15,marginBottom:5,fontWeight:"bold" }}   >{item.gyakorlat_nev} </Text>
+          <Image  source={{uri: 'http://192.168.1.106:3000/'+item.gyakorlat_kep}} style={{width:300,height:300,marginLeft:"auto",marginRight:"auto"}} />  
+          <Text style={{color:"brown",fontSize:16,textAlign:"center",marginTop:15,marginBottom:5,textAlign:"justify"}}   >{item.gyakorlat_leiras} </Text>
+          
+          </View>
+        
+        }
+
+        
+          
+        />
       </View>
-      
-      
     );
   }
 }
+
+const styles = StyleSheet.create({
+  
+  kekgomb: {
+    alignItems: "center",
+    backgroundColor: "blue",
+    padding: 10,
+    width:300,
+    marginLeft:"auto",
+    marginRight:"auto",
+  }
+});
