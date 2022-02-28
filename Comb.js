3cd20d2f9a960193a901d51e01839d8a170c6006
tanulo <tanulo@barossgabor.local>
2022-02-28 12:27:43 +0100

    02.28

--- Comb.js
+++ Comb.js
@@ -47,9 +47,9 @@ export default class Comb extends React.Component {
           renderItem={({item}) => 
 
           /*<View style={{ border: "solid blue",width:600, marginLeft:"auto",marginRight:"auto",padding:20,marginBottom:10,borderRadius:20,}}>*/
-          <View style={{marginLeft:"auto",marginRight:"auto",padding:40,marginBottom:10,  border: "solid blue", borderRadius:60,backgroundColor: "#B9F3EA"}}>
+          <View style={{marginLeft:"auto",marginRight:"auto",padding:10,marginBottom:10,  border: "solid blue", borderRadius:60,backgroundColor: "#B9F3EA"}}>
           <Text style={{color:"brown",fontSize:40,textAlign:"center",marginTop:15,marginBottom:5,fontWeight:"bold" }}   >{item.gyakorlat_nev} </Text>
-          <Image  source={{uri: 'http://192.168.2.112:3000/'+item.gyakorlat_kep}} style={{width:300,height:300,marginLeft:"auto",marginRight:"auto"}} />  
+          <Image  source={{uri: 'http://192.168.2.112:3000/'+item.gyakorlat_kep}} style={{width:370,height:300,marginLeft:"auto",marginRight:"auto"}} />  
           <Text style={{color:"brown",fontSize:16,textAlign:"center",marginTop:15,marginBottom:5,textAlign:"justify"}}   >{item.gyakorlat_leiras} </Text>
           
           </View>
--- Fooldal.js
+++ Fooldal.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {StyleSheet, FlatList, ActivityIndicator, Text, View, Image , TouchableOpacity } from 'react-native';
-import Collapsible from 'react-native-collapsible';
-import Drawer from './App';
+import Iframe from 'react-iframe';
 
 export default class Mell extends React.Component {
 
@@ -47,14 +46,20 @@ export default class Mell extends React.Component {
           data={this.state.dataSource}
           renderItem={({item}) => 
 
-          <TouchableOpacity onPress={{}}>
-          <View style={{marginLeft:"auto",marginRight:"auto",padding:40,marginBottom:10,  border: "solid blue", borderRadius:60,backgroundColor: "#B9F3EA"}}>
+       
+          <View style={{marginLeft:"auto",marginRight:"auto",padding:10,marginBottom:10,  border: "solid blue", borderRadius:20,backgroundColor: "#B9F3EA"}}>
           <Text style={{color:"brown",fontSize:40,textAlign:"center",marginTop:15,marginBottom:5,fontWeight:"bold" }}   >{item.izom_nev} </Text>
-          <Image  source={{uri: 'http://192.168.2.112:3000/'+item.izom_kep}} style={{width:300,height:300,marginLeft:"auto",marginRight:"auto"}} />  
-          
-
-          </View>
-        </TouchableOpacity>
+          <Image  source={{uri: 'http://192.168.2.112:3000/'+item.izom_kep}} style={{width:370,height:300,marginLeft:"auto",marginRight:"auto"}} />  
+        {/*<Iframe url="http://www.youtube.com/embed/rsWw-JN46Xw"
+        width="450px"
+        height="450px"
+        id="myId"
+        className="myClassname"
+        display="initial"
+    position="relative"/>*/}
+         
+         
+       </View> 
         }
 
         
